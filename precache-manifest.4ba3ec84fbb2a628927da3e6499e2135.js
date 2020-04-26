65651f328bfa0e570192ec59a2a1a222ceb4234b
anshuman2051 <anshumansingh2051@gmail.com>
2020-04-26 15:19:17 +0530

    Updates

---
+++ precache-manifest.4ba3ec84fbb2a628927da3e6499e2135.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29383804e2cd918ad523dd2f8d572618",
+    "url": "/ipl-mania/index.html"
+  },
+  {
+    "revision": "2001a07e85b784c27843",
+    "url": "/ipl-mania/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "02144275bb716213b907",
+    "url": "/ipl-mania/static/css/main.a0465362.chunk.css"
+  },
+  {
+    "revision": "2001a07e85b784c27843",
+    "url": "/ipl-mania/static/js/2.16d7449b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ipl-mania/static/js/2.16d7449b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02144275bb716213b907",
+    "url": "/ipl-mania/static/js/main.2aea89f1.chunk.js"
+  },
+  {
+    "revision": "87322e2fe3ac5096de80",
+    "url": "/ipl-mania/static/js/runtime-main.2f721272.js"
+  }
+]);
